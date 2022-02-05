1df8b27d4692e2225c080ecdcb1df81c10a6a657
SnkrFr3sh <mpacampara@gmail.com>
2022-02-04 16:06:02 -0800

    added scripts in package.json

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,20 +14,22 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-console.error('putDb not implemented');
+// console.error('putDb not implemented');
 const jateDb = await openDB('jate', 1);
 const tx = jateDb.transaction('jate','readwrite')
 const store = tx.objectStore('jate');
-const request = store.put({id: id, todo:content});
+const request = store.put({jate: content});
 const results = await request;
-console.log('🚀 - data saved to the database', result);
+console.log('🚀 - data saved to the database', results);
 };
 
 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {
-console.error('getDb not implemented');
+// export const getDb = async () => 
+
+export const getDb = async (id) => {
+// console.error('getDb not implemented');
 console.log('GET from the database');
 const jateDb = await openDB('jate', 1);
 const tx = jateDb.transaction('jate', 'readonly');
